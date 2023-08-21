2c48e7d3db821d94cbbd68516d813ddcc66cafd0
ThisAster <freizen034@gmail.com>
2023-08-22 02:55:51 +0300

    create project

---
+++ main.cpp
@@ -0,0 +1,157 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+int ground = 400;
+float offsetX = 0, offsetY = 0;
+
+
+const int H = 12;
+const int W = 40;
+
+
+String TileMap[H] = {
+
+"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+"B                                B     B",
+"B                                B     B",
+"B                                B     B",
+"B                                B     B",
+"B         0000                BBBB     B",
+"B                                B     B",
+"BBB                              B     B",
+"B              BB                BB    B",
+"B              BB                      B",
+"B    B         BB         BB           B",
+"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+
+};
+
+class PLAYER {
+public:
+    float dx, dy; // скорость
+    FloatRect rect; // размер персонажа
+    bool onGround;
+    Sprite sprite; // картинка персонажа
+    float currentFrame;
+
+    PLAYER(Texture& image)
+    {
+        sprite.setTexture(image);
+        rect = FloatRect(7 * 32, 9 * 32, 40, 50);
+
+        dx = dy = 0.1;
+        currentFrame = 0;
+    }
+
+    void update(float time)
+    {
+        rect.left += dx * time;
+        CollisionX();
+        if (!onGround) dy = dy + 0.0005 * time;
+        rect.top += dy * time;
+        onGround = false;
+        CollisionY();
+        if (rect.top > ground) { rect.top = ground; dy = 0; onGround = true; }
+        currentFrame += 0.005 * time;
+        if (currentFrame > 6) currentFrame -= 6;
+        if (dx > 0) sprite.setTextureRect(IntRect(40 * int(currentFrame), 244, 40, 50));
+        if (dx < 0) sprite.setTextureRect(IntRect(40 * int(currentFrame) + 40, 244, -40, 50));
+        sprite.setPosition(rect.left, rect.top);
+
+        dx = 0;
+    }
+
+    void CollisionX()
+    {
+        for (int i = rect.top/32; i<(rect.top + rect.height)/32; i++)
+            for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++)
+            {
+                if (TileMap[i][j] == 'B')
+                {
+                    if (dx > 0) rect.left = j * 32 - rect.width;
+                    if (dx < 0) rect.left = j * 32 + 32;
+                }
+            }
+    }
+
+
+    void CollisionY()
+    {
+        for (int i = rect.top / 32; i < (rect.top + rect.height) / 32; i++)
+            for (int j = rect.left / 32; j < (rect.left + rect.width) / 32; j++)
+            {
+                if (TileMap[i][j] == 'B')
+                {
+                    if (dy > 0) { rect.top = i * 32 - rect.height; dy = 0; onGround = true; }
+                    if (dy < 0) { rect.left = i * 32 + 32; dy = 0;}
+                }
+            }
+    }
+};
+
+int main()
+{
+    RenderWindow window(VideoMode(600, 450), "SFML works!");
+    Texture t;
+    t.loadFromFile("fang.png");
+
+    float currentFrame = 0;
+
+    PLAYER p(t);
+    Clock clock;
+
+    RectangleShape rectangle(Vector2f(32, 32));
+
+    while (window.isOpen())
+    {
+        float time = clock.getElapsedTime().asMicroseconds();
+        clock.restart();
+        time = time / 700;
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed)
+                window.close();
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            p.dx = -0.1;
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            p.dx = 0.1;
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Up))
+        {
+            if (p.onGround) { p.dy = -0.35; p.onGround = false; }
+        }
+
+        p.update(time);
+
+
+        window.clear(Color::White);
+
+        for (int i = 0; i < H; i++)
+            for (int j = 0; j < W; j++)
+            {
+                if (TileMap[i][j] == 'B') rectangle.setFillColor(Color::Black);
+
+                if (TileMap[i][j] == '0')  rectangle.setFillColor(Color::Green);
+
+                if (TileMap[i][j] == ' ') continue;
+
+                rectangle.setPosition(j * 32, i * 32);
+                window.draw(rectangle);
+            }
+
+
+
+        window.draw(p.sprite);
+        window.display();
+    }
+
+    return 0;
+}
